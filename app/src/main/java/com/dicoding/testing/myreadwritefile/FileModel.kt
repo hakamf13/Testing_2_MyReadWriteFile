0f449243857c6afaddfb0edc40422732ce1484a0
Muhammad Hakam Fardana <mhakamf13@gmail.com>
2022-09-30 22:31:41 +0700

    Finish code for a simple notepad - Data Storage

---
+++ app/src/main/java/com/dicoding/testing/myreadwritefile/FileModel.kt
@@ -0,0 +1,6 @@
+package com.dicoding.testing.myreadwritefile
+
+data class FileModel(
+    var filename: String? = null,
+    var data: String? = null
+)
--- app/src/main/java/com/dicoding/testing/myreadwritefile/MainActivity.kt
+++ app/src/main/java/com/dicoding/testing/myreadwritefile/MainActivity.kt
@@ -2,10 +2,73 @@ package com.dicoding.testing.myreadwritefile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.dicoding.testing.myreadwritefile.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNew.setOnClickListener(this)
+        binding.buttonOpen.setOnClickListener(this)
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_new -> newFile()
+            R.id.button_open -> showList()
+            R.id.button_save -> saveFile()
+        }
+    }
+
+    private fun newFile() {
+        binding.editTitle.setText("")
+        binding.editFile.setText("")
+        Toast.makeText(this, " Clearing file", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showList() {
+        val items = fileList()
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Choose file")
+        builder.setItems(items) { dialog, item -> loadData(items[item].toString()) }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun loadData(title: String) {
+        val fileModel = FileHelper.readFromFile(this, title)
+        binding.editTitle.setText(fileModel.filename)
+        binding.editFile.setText(fileModel.data)
+        Toast.makeText(this, "Loading " + fileModel.filename + " data", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun saveFile() {
+        when {
+            binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "You must fill the Title!", Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() -> Toast.makeText(this, "You must fill the Content!", Toast.LENGTH_SHORT).show()
+            else -> {
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+                val fileModel = FileModel()
+                fileModel.filename = title
+                fileModel.data = text
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving " + fileModel.filename + " file", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
